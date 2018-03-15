5892105bf7b558482aa0c3844a5abc55ea3cf815
aksoyhasan <aksoyhasan@github.com>
2018-03-15 15:26:03 +0300

    DB Class Added

---
+++ Restaurant Application/DB Layer/DataAccessLayer.cs
@@ -0,0 +1,21 @@
+﻿using Restaurant_Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Application.DB_Layer
+{
+    class DataAccessLayer
+    {
+        internal IEnumerable<object> GetFoodItems()
+        {
+            throw new NotImplementedException();
+        }
+        public ICollection<FoodItems> GetFoodItems()
+        {
+            return _rDBContext.FoodItems.OrderBy(p => p.FoodID).ToArray();
+        }
+    }
+}
